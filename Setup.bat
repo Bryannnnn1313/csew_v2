715b1de05a69d0efe07405d77028f343faed3679
Shaun <shaunmartin1998@yahoo.com>
2020-07-10 13:11:54 -0700

    Cleanup

--- Setup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-Powershell.exe -NoProfile -Command "& {Start-Process Powershell.exe -Argumentlist '-NoProfile -ExecutionPolicy Bypass -File "%~dp0\Install-Python.ps1"' -Verb RunAs}
-echo Once Python is Finished Press Any Key.
-pause > nul
-pip install pypiwin32
